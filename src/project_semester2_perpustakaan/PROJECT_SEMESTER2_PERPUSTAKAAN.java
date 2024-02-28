65a2c6c817dd4c0981216c8b2b425954b1aacecc
Harsya1 <dika20949@gmail.com>
2024-02-28 09:28:33 +0700

    First Commit

---
+++ src/project_semester2_perpustakaan/PROJECT_SEMESTER2_PERPUSTAKAAN.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package project_semester2_perpustakaan;
+
+/**
+ *
+ * @author dika2
+ */
+public class PROJECT_SEMESTER2_PERPUSTAKAAN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
